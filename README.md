6e9864fa835ce9808eccdf88081937f53ea4631a
chanchann <www.769440972@qq.com>
2022-07-30 11:40:45 +0800

    update install.sh

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# vim_config
+# Steps
+
+1. clone this repo
+
+
+
+2. run instsall scrpt
+
+cd vim_config
+
+./install.sh
+
+3. install plugin
+
+4. configure ccls
+
--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 echo "----------------install nvim----------------"
 sudo apt-get install neovim
 
-echo "----------------install Vim-Plug----------------"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir ~/.config/nvim/
-nvim ~/.config/nvim/init.vim
-
 echo "----------------install ctags----------------"
 sudo apt-get install install ctags
 
@@ -20,3 +14,15 @@ sudo apt-get install yarn
 echo  "----------------install fzf---------------"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+echo "----------------install Vim-Plug----------------"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if [ ! -d ~/.config/nvim/ ]; then
+    mkdir -p ~/.config/nvim/
+fi
+
+rm -f ~/.config/nvim/init.vim
+cp init.vim ~/.config/nvim/init.vim
+
+source ~/.bashrc
